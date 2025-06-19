b3f964db14d48889bd4dcc8411e66f105f1cee32
kerem-acar <kerem.acar789@gmail.com>
2025-06-19 11:23:08 -0700

    Finished react components for login and register pages

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,8 +1,68 @@
+import React, {useState} from 'react';
+import axios from "axios";
+import {Link} from "react-router-dom";
+
 function Login (){
+    const [text, setText] = useState("");
+    const [text1, setText1] = useState("");
+    const [showFail, setshowFail] = useState(false);
+    const [showSuccess, setshowSuccess] = useState(false);
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleChange1 = (e) => {
+        setText1(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        try{
+        e.preventDefault()
+        const postResponse = await axios.post("http://127.0.0.1:5000/api/login/", { username: text, password: text1 })
+        setText("");
+        setText1("");
+        setshowSuccess(true);
+        }
+        catch{
+            setshowFail(true);
+        }
+    };
+
+    const handleFail = () => {
+        setshowFail(false);
+        setText("");
+        setText1("");
+    };
+
+    
     return (
         <div>
-
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input value={text} placeholder="Username" onChange={handleChange}></input>
+                <input value={text1} placeholder="Password" onChange={handleChange1}></input>
+                <button type='submit'>Log In</button>
+            </form>
+            <div className={showFail === false ? 'loginfail-container' : 'loginfail-container-open'}>
+                <div>
+                    <h2>Try Again!</h2>
+                    <button onClick={handleFail}>Ok</button>
+                </div>
+            </div>
+            <div className={showSuccess === false ? 'loginsuccess-container' : 'loginsuccess-container-open'}>
+                <div>
+                    <h2>Success!</h2>
+                    <button>
+                        <Link to="/">Ok</Link>
+                    </button>
+                </div>
+            </div>
         </div>
+
+        
+        
+        
 
     );
 
--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -1,8 +1,49 @@
+import React, {useState} from 'react';
+import axios from "axios";
+import {Link} from "react-router-dom";
+
 function Register (){
+    const [text, setText] = useState("");
+    const [text1, setText1] = useState("");
+    const [showWelcome, setshowWelcome] = useState(false);
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const handleChange1 = (e) => {
+        setText1(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const postResponse = await axios.post("http://127.0.0.1:5000/api/register/", { username: text, password: text1 })
+        setText("");
+        setText1("");
+        setshowWelcome(true);
+    }
+    
     return (
         <div>
-
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <input value={text} placeholder="Username" onChange={handleChange}></input>
+                <input value={text1} placeholder="Password" onChange={handleChange1}></input>
+                <button type='submit'>Register</button>
+            </form>
+            <div className={showWelcome === false ? 'welcome-container' : 'welcome-container-open'}>
+                <div>
+                    <h2>Welcome!</h2>
+                    <button>
+                        <Link to="/">Ok</Link>
+                    </button>
+                </div>
+            </div>
         </div>
+
+        
+        
+        
 
     );
 
